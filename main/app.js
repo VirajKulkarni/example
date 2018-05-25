748eb8b0075106a3f48688dd78aa9378120fc7da
Viraj Kulkarni <viraj.kulkarni@stud.tu-darmstadt.de>
2018-05-25 23:51:21 +0200

    Config changes

--- main/app.js
+++ main/app.js
@@ -5,14 +5,17 @@ const app        = express();
 const bodyParser = require('body-parser');
 const logger 	   = require('morgan');
 const router 	   = express.Router();
-const port 	   = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+//const port 	   = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
+console.log("router"+router);
 
 require('./routes')(router);
 app.use('/api/v1', router);
 
-app.listen(port);
+app.listen(port,ip);
 
-console.log(`flashmob App is Running on ${port}`);
+console.log(`flashmob App is Running on ${port} ${ip} `);
--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL;
-const db_name = "Flashmob";
+//const db_name = "Flashmob";
 
 const userSchema = mongoose.Schema({ 
 
@@ -19,11 +19,32 @@ const userSchema = mongoose.Schema({
 
 mongoose.Promise = global.Promise;
 
-if(process.env.OPENSHIFT_MONGODB_DB_URL){
-	mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
-}
-else{
-	mongoURL = 'mongodb://localhost:27017/' + db_name;
+//if(process.env.OPENSHIFT_MONGODB_DB_URL){
+//	mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+//}
+
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+	var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+	    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+	    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+	    mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+	    mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+	    mongoUser = process.env[mongoServiceName + '_USER'];
+      
+	if (mongoHost && mongoPort && mongoDatabase) {
+	  mongoURLLabel = mongoURL = 'mongodb://';
+	  if (mongoUser && mongoPassword) {
+	    mongoURL += mongoUser + ':' + mongoPassword + '@';
+	  }
+	  // Provide UI label that excludes user id and pw
+	  mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+	  mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+      
+	}
+      }
+      else{
+	mongoURL = 'mongodb://localhost:27017/Flashmob';
 }
 
 mongoose.connect(mongoURL);
